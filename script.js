fe47251ae511cd2c4bf2d7bcf5646791db3161fb
Elizabeth-Bakul <bakelal@mail.ru>
2021-05-14 20:57:29 +0300

    Diplom

--- script.js
+++ script.js
@@ -41,7 +41,6 @@ window.addEventListener("DOMContentLoaded", () => {
     overlay.addEventListener("click", (e) => {
       let target = e.target;
       // если нажимаем кнопку "Закрыть" или нажимаем вне области окна, то закрываем его
-      console.log(target);
       if (
         target.classList.contains("modal-close") ||
         (!target.closest(".modal-callback"))
@@ -53,23 +52,24 @@ window.addEventListener("DOMContentLoaded", () => {
   };
   togglePopUp("callback-btn");
   togglePopUp("fancyboxModal");
-  const topMenu = document.querySelector('.top-menu');
-  const scrollLinks = topMenu.querySelectorAll("a");
+  const scrollLink = ()=> {
+    const topMenu = document.querySelector(".top-menu");
+    const scrollLinks = topMenu.querySelectorAll("a");
 
-  for (const scrollLink of scrollLinks) {
-    scrollLink.addEventListener("click", (event) => {
-      event.preventDefault();
-      const id = scrollLink.getAttribute("href");
-      console.log(id)
-      if (id !== "#callback") {
-        console.log("scroll");
-        document.querySelector(id).scrollIntoView({
-          behavior: "smooth",
-          block: "start",
-        });
-      }
-    });
+    for (const scrollLink of scrollLinks) {
+      scrollLink.addEventListener("click", (event) => {
+        event.preventDefault();
+        const id = scrollLink.getAttribute("href");
+        if (id !== "#callback") {
+          document.querySelector(id).scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+          });
+        }
+      });
+    }
   }
+  scrollLink();
 
   const toggleSlider = () => {
     const slides = document.querySelectorAll(".item"),
@@ -122,14 +122,12 @@ window.addEventListener("DOMContentLoaded", () => {
 
     // запуск слайдшоу
     const startAutoPlay = () => {
-      console.log("start");
       idInterval = setInterval(autoPlaySlider, 3000);
     };
     startAutoPlay();
 
     // останов слайдшоу
     const stopAutoPlay = () => {
-      console.log("stop");
       clearInterval(idInterval);
     };
 
@@ -185,5 +183,129 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   };
   toggleSlider();
-  
+  const toogleAccordeon = ()=>{
+    let accordeon=document.querySelector('.accordeon');
+    let listItem = accordeon.querySelectorAll('.element');
+    listItem.forEach((element)=>{
+      element.addEventListener("click", function() {
+        let text = this.querySelector(".element-content");
+        if (this.classList.contains("active")){
+          this.classList.remove("active");
+          text.style.display = "none";
+        } else {
+          for(el of listItem){
+            el.classList.remove("active");
+            let content = el.querySelector(".element-content");
+            content.style.display = "none";
+          }
+          this.classList.add("active");
+          text.style.display = "block";
+        }
+      });
+    });
+  }
+  toogleAccordeon();
+
+  const scrollButton = ()=>{
+    const trackScroll = () => {
+      let scrolled = window.pageYOffset;
+      let coordHeader = document.querySelector(".header-wrapper").clientHeight;
+      let coordSlider = document.querySelector(".top-slider").clientHeight;
+      let coords = coordHeader + coordSlider;
+      if (scrolled > coords) {
+        goTopBtn.classList.add("up-show");
+      }
+      if (scrolled < coords) {
+        goTopBtn.classList.remove("up-show");
+      }
+    };
+    const backToTop = () => {
+      document.querySelector('body').scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    };
+
+    let goTopBtn = document.querySelector(".up");
+    trackScroll();
+    window.addEventListener("scroll", trackScroll);
+    goTopBtn.addEventListener("click", backToTop);
+
+    
+  }
+  scrollButton();
+
+  const sendForm = (formIdString) => {
+    const errorMessage = "Ошибка",
+      loadMessage = "Загрузка...",
+      successMessage = "Спасибо! Мы скоро с вами свяжемся!";
+
+    const form = document.getElementById(formIdString), // форма для заполнения заявки
+      statusMessageElement = document.createElement("div"), // сообщение о статусе отправки заявки
+      inputs = document.querySelectorAll(`#${formIdString} input`); // все inputs из формы
+
+    statusMessageElement.style.cssText = "text-align: center; color: black";
+
+    // Функция отправки данных формы на сервер
+    const postData = (requestBody) => {
+      return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+        request.addEventListener("readystatechange", () => {
+          if (request.readyState !== 4) {
+            return;
+          }
+          if (request.status === 200) {
+            resolve();
+          } else {
+            reject(request.statusText);
+          }
+        });
+        request.open("POST", "server.php");
+        request.setRequestHeader("Content-Type", "application/json");
+        request.send(JSON.stringify(requestBody));
+      });
+    };
+
+    // Валидация данных при вводе телефона
+    form.addEventListener("input", (e) => {
+      if (e.target.matches('input[name="tel"]')) {
+        e.target.setAttribute("pattern", "^[0-9+]{11,18}$");
+        e.target.value = e.target.value.replace(/[^\d\+]/g, "");
+      }
+    });
+
+
+    // Валидация данных при вводе имени
+    form.addEventListener("input", (e) => {
+      if (e.target.matches('input[name="fio"]')) {
+        e.target.setAttribute("pattern", "[А-Яа-яЁё ]{2,}");
+        e.target.value = e.target.value.replace(/[^А-Яа-яЁё\s]|/g, "");
+      }
+    });
+
+    // Слушатель формы
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      form.append(statusMessageElement);
+
+      statusMessageElement.textContent = loadMessage;
+
+      const formData = new FormData(form);
+      let body = {};
+
+      // берем данные формы и заполняем тело запроса body
+      formData.forEach((item, index) => (body[index] = item)); // у объекта formData есть свой метод forEach()
+
+      // отправка данных и уведомление пользователя
+      postData(body)
+        .then(() => (statusMessageElement.textContent = successMessage))
+        .catch((error) => {
+          statusMessageElement.textContent = errorMessage;
+          console.log(error);
+        });
+
+      inputs.forEach((item) => (item.value = "")); // очистка input после отправки данных
+    });
+  };
+  sendForm("form1");
 });
